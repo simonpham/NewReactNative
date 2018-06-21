377901d00b44790ba884c39809ad4b166b911a59
Oanh Huynh <thebkviphxo@gmail.com>
2018-06-21 15:03:14 +0700

    Add files via upload
    simple onboarding screen

---
+++ App.js
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, StatusBar, Text, View } from "react-native";
+import { StackNavigator } from "react-navigation";
+
+import Screen from "./app/components/Screen";
+import Swiper from "./app/components/Swiper";
+import Boiler from "./app/components/Boiler";
+
+/*const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
+});*/
+
+class Home extends Component {
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: "#16a085",
+      elevation: null
+    },
+    header: null
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" backgroundColor="#16a085" />
+        <Screen navigation={this.props.navigation} />
+      </View>
+    );
+  }
+}
+
+export default App = StackNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      title: "Home"
+    }
+  },
+  Screen: {
+    screen: Screen,
+    navigationOptions: {
+      title: "Screen"
+    }
+  },
+  Swiper: {
+    screen: Swiper,
+    navigationOptions: {
+      title: "Swiper"
+    }
+  },
+  Boiler: {
+    screen: Boiler,
+    navigationOptions: {
+      title: "Boiler"
+    }
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
